cb317d2c53910a918c59fe3fe61cdd6e1cc556cc
dzm <vvv-rrrr@list.ru>
2017-07-12 22:54:52 +0300

    code

--- BubbleSort/BubbleSortOfArray/BubbleSort.cs
+++ BubbleSort/BubbleSortOfArray/BubbleSort.cs
@@ -7,142 +7,30 @@ using System.Threading.Tasks;
 namespace BubbleSortOfArray
 {
     /// <summary>
-    /// delegate of compare
+    /// interface provides method for compare two array
     /// </summary>
-    /// <param name="lhs">first array</param>
-    /// <param name="rhs">second array</param>
-    /// <returns>number</returns>
-    public delegate int Compare(int[] lhs, int[] rhs);
+    public interface ICompare
+    {
+        int CompareTo(int[] lhs, int[] rhs);
+    }
     /// <summary>
     /// class BubbleSort of array
     /// </summary>
     public static class BubbleSortArray
     {
-        #region BubbleSort
         /// <summary>
         /// BubbleSort
         /// </summary>
         /// <param name="array">array with 2 measure</param>
-        public static void BubbleSort(int[][] array, string namefunc)
+        public static void BubbleSort(int[][] array, ICompare compare)
         {
-
-            /* Console.WriteLine("Enter: maxI, minI,...");
-             switch (namefunc)
-             {
-                 case "maxI":
-                     compare = new Compare (ByMaxElementIncrement);
-                     break;
-                 case "sumI":
-                     compare = BySumOfArrayIncrement;
-                     break;
-                 case "minI":
-                     compare = ByMinElementIncrement;
-                     break;
-                 case "maxD":
-                     compare = ByMaxElementDecrease;
-                     break;
-                 case "sumD":
-                     compare = BySumOfArrayDecrease;
-                     break;
-                 case "minD":
-                     compare = ByMinElementDecrease;
-                     break;
-                 default:
-                     Console.WriteLine("Default case");
-                     break;
-             }*/
-            Compare compare = BySumOfArrayIncrement;
             for (int i = 0; i < array.Length; i++) 
             {
                 for(int j = 0; i < array.Length - 1 - i; i++)
                 {
-                    if (compare(array[j], array[j + 1]) > 0) Swap(ref array[j], ref array[j+1]);  
+                    if (compare.CompareTo(array[j], array[j + 1]) > 0) Swap(ref array[j], ref array[j + 1]);
                 }
             }
-        }
-        #endregion
-
-        #region IncrementOrDecrease
-        /// <summary>
-        /// sum of array Increment
-        /// </summary>
-        /// <param name="lhs">first array</param>
-        /// <param name="rhs">second array</param>
-        /// <returns>number</returns>
-        private static int BySumOfArrayIncrement(int[] lhs, int[] rhs) => SumOfArray(lhs) - SumOfArray(rhs);
-        /// <summary>
-        /// sum of array Increment
-        /// </summary>
-        /// <param name="lhs">first array</param>
-        /// <param name="rhs">second array</param>
-        /// <returns>number</returns>
-        private static int ByMinElementIncrement(int[] lhs, int[] rhs) => FindMinElement(lhs) - FindMinElement(rhs);
-        /// <summary>
-        /// Max Element Increment
-        /// </summary>
-        /// <param name="lhs">first array</param>
-        /// <param name="rhs">second array</param>
-        /// <returns>number</returns>
-        private static int ByMaxElementIncrement(int[] lhs, int[] rhs) => FindMaxElement(lhs) - FindMaxElement(rhs);
-        /// <summary>
-        /// sum of array decrease
-        /// </summary>
-        /// <param name="lhs">first array</param>
-        /// <param name="rhs">second array</param>
-        /// <returns>number</returns>
-        private static int BySumOfArrayDecrease(int[] lhs, int[] rhs) => -SumOfArray(lhs) + SumOfArray(rhs);
-        /// <summary>
-        /// Min Elemen Decrease
-        /// </summary>
-        /// <param name="lhs">first array</param>
-        /// <param name="rhs">second array</param>
-        /// <returns>number</returns>
-        private static int ByMinElementDecrease(int[] lhs, int[] rhs) => -FindMinElement(lhs) + FindMinElement(rhs);
-        /// <summary>
-        /// Max Element Decrease
-        /// </summary>
-        /// <param name="lhs">first array</param>
-        /// <param name="rhs">second array</param>
-        /// <returns>number</returns>
-        private static int ByMaxElementDecrease(int[] lhs, int[] rhs) => -FindMaxElement(lhs) + FindMaxElement(rhs);
-        #endregion
-
-        #region AditionMethod
-        /// <summary>
-        /// sum of array
-        /// </summary>
-        /// <param name="arr">array</param>
-        /// <returns>number</returns>
-        private static int SumOfArray(int[] arr)
-        {
-            int sum = 0;
-            foreach (int el in arr)
-                sum += el;
-            return sum;
-        }
-        /// <summary>
-        /// Max Element
-        /// </summary>
-        /// <param name="arr">array</param>
-        /// <returns>number</returns>
-        private static int FindMaxElement(int[] arr)
-        {
-            int max = arr[0];
-            for (int i = 0; i < arr.Length; i++)
-                if (max < arr[i]) max = arr[i];
-            return max;
-        }
-        /// <summary>
-        /// Min Element
-        /// </summary>
-        /// <param name="arr">array</param>
-        /// <returns>number</returns>
-        private static int FindMinElement(int[] arr)
-        {
-            int min = arr[0];
-            for (int i = 0; i < arr.Length; i++)
-                if (min > arr[i]) min = arr[i];
-            return min;
         }
         /// <summary>
         /// swap of array
@@ -155,6 +43,5 @@ namespace BubbleSortOfArray
             a = b;
             b = t;
         }
-        #endregion
     }
 }
--- BubbleSort/NUnitBubbleSort.Tests1/TestBubbleSort.cs
+++ BubbleSort/NUnitBubbleSort.Tests1/TestBubbleSort.cs
@@ -4,16 +4,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BubbleSortOfArray;
 
 namespace NUnitBubbleSort.Tests1
 {
     [TestFixture]
     public class TestBubbleSort
-    {
-        //[TestCase(new int[5][4]{ }{ })]
-        public void Test_BubbleSort(int[][] array)
+    {    
+        [TestCase(new int[] { 1, 2, 3 }, new int[] { 2, 3, 3 }, new int[] { 0, 0, 0 }, ExpectedResult = true)]
+        public bool Test_BubbleSort(params int[][] array)
         {
-
+            ICompare refe = new ByMinElementIncrement();
+            BubbleSortArray.BubbleSort(array, refe);
+            int[,] myArr = { { 0, 0, 0 }, { 1, 2, 3 }, { 2, 3, 3 } };
+            return Array.Equals(array, myArr);
         }
     }
+    class BySumOfArrayIncrement : ICompare
+    {
+        public int CompareTo(int[] lhs, int[] rhs)=>lhs.Sum() - rhs.Sum();
+    }
+    class ByMinElementIncrement : ICompare
+    {
+        public int CompareTo(int[] lhs, int[] rhs) => lhs.Min() - rhs.Min();
+    }
+    class ByMaxElementIncrement : ICompare
+    {
+        public int CompareTo(int[] lhs, int[] rhs) => lhs.Max() - rhs.Max();
+    }
+    class BySumOfArrayDecrease : ICompare
+    {
+        public int CompareTo(int[] lhs, int[] rhs) => -lhs.Sum() + rhs.Sum();
+    }
+    class ByMaxElementDecrease : ICompare
+    {
+        public int CompareTo(int[] lhs, int[] rhs) => -lhs.Max() + rhs.Max();
+    }
+    class ByMinElementDecrease : ICompare
+    {
+        public int CompareTo(int[] lhs, int[] rhs) => lhs.Min() - rhs.Min();
+    }
+
 }
